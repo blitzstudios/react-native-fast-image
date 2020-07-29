20ce4384ade40dc5e2c636480408402b770c6a78
Luke Herbold <luke@sleeper.app>
2020-07-29 11:58:26 -0700

    Adding loopCount property to specify the number of times a gif/webp animation should repeat.

--- android/src/main/java/com/dylanvann/fastimage/FastImageViewWithUrl.java
+++ android/src/main/java/com/dylanvann/fastimage/FastImageViewWithUrl.java
@@ -5,11 +5,17 @@ import android.content.Context;
 import androidx.appcompat.widget.AppCompatImageView;
 
 import com.bumptech.glide.load.model.GlideUrl;
+import com.bumptech.glide.load.resource.gif.GifDrawable;
 
 class FastImageViewWithUrl extends AppCompatImageView {
     public GlideUrl glideUrl;
+    public int loopCount = GifDrawable.LOOP_INTRINSIC;
 
     public FastImageViewWithUrl(Context context) {
         super(context);
+    }
+
+    public void setLoopCount(int loop) {
+        loopCount = loop;
     }
 }
